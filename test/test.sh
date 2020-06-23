cc604897d75a884feb4fa5ea87666785da822935
adrienjt <adrienjt@users.noreply.github.com>
2020-06-23 14:28:50 -0700

    verify code gen
    Signed-off-by: adrienjt <adrienjt@users.noreply.github.com>

--- test/test.sh
+++ test/test.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# TODO check that hack/codegen.sh doesn't change any files (check output or git status afterward)
-
+./hack/verify-codegen.sh
 go vet ./pkg/... ./cmd/...
 go test -v ./pkg/... ./cmd/... # -coverprofile cover.out
 # TODO save cover.out somewhere
