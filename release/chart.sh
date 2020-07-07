5e358c928aed4293406ee6f8dfe87439d7d7225d
adrienjt <adrienjt@users.noreply.github.com>
2020-07-07 10:17:51 -0700

    release CRDs separately, esp. for helm upgrade
    Signed-off-by: adrienjt <adrienjt@users.noreply.github.com>

--- release/chart.sh
+++ release/chart.sh
@@ -5,4 +5,7 @@ helm package charts/multicluster-scheduler -d _out
 cp charts/index.yaml _out/
 helm repo index _out --merge _out/index.yaml --url https://charts.admiralty.io
 
+# release CRDs separately, esp. for `helm upgrade`
+cat charts/multicluster-scheduler/crds/* > _out/admiralty.crds.yaml
+
 # TODO: upload Helm chart and new index
