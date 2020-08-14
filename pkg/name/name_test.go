a030e30f6e214a129da8108fb32b9c177b36707d
adrienjt <adrienjt@users.noreply.github.com>
2020-08-14 15:09:06 -0700

    add unit tests
    Signed-off-by: adrienjt <adrienjt@users.noreply.github.com>

--- pkg/name/name_test.go
+++ pkg/name/name_test.go
@@ -20,38 +20,82 @@ import (
 	"testing"
 )
 
-func TestFromNamespaceName(t *testing.T) {
-	cases := []struct{ namespace, name, expected, description string }{
+func Test(t *testing.T) {
+	cases := []struct {
+		constantIndices []int
+		optionalIndices []int
+		parts           []string
+		expected        string
+		description     string
+	}{
 		{
-			"default",
-			"foo",
-			"default-foo-947c8f7",
-			"normal",
+			nil,
+			nil,
+			[]string{"default", "foo"},
+			"default-foo",
+			"no hyphen",
 		},
 		{
-			"verylongnamespace-verylongnamespace-verylongnamespace",
-			"verylongname",
-			"verylongnamespace-verylongnamespace-verylongnamespace-v-9190b58",
+			nil,
+			nil,
+			[]string{"default", "foo-bar"},
+			"default-foo-bar-b1f5f88a38",
+			"with hyphen",
+		},
+		{
+			nil,
+			nil,
+			[]string{"default-foo", "bar"},
+			"default-foo-bar-ce0b03a405",
+			"with hyphen, different hash",
+		},
+		{
+			nil,
+			nil,
+			[]string{"verylongnamespaceverylongnamespace", "verylongnameverylongnameverylongname"},
+			"verylongnamespaceverylongnamespace-verylongnameveryl-79f7165434",
 			"too long",
 		},
 		{
-			"verylongnamespace-verylongnamespace-verylo",
-			"verylongname",
-			"verylongnamespace-verylongnamespace-verylo-verylongname-274a70c",
-			"at the limit",
+			nil,
+			nil,
+			[]string{"verylongnamespaceverylongnamespace", "verylongnamevery-longnameverylongname"},
+			"verylongnamespaceverylongnamespace-verylongnamevery-f098147021", // 62 characters long
+			"remove trailing hyphen",
 		},
 		{
-			"verylongnamespace-verylongnamespace-verylong",
-			"very-long-name",
-			"verylongnamespace-verylongnamespace-verylong-very-long-8cddda0",
-			"remove trailing dash",
+			[]int{0},
+			nil,
+			[]string{"prefix", "foo-bar"},
+			"prefix-foo-bar",
+			"single variable part with hyphen",
+		},
+		{
+			nil,
+			[]int{0},
+			[]string{"", "foo"},
+			"foo",
+			"single non-optional part",
+		},
+		{
+			nil,
+			[]int{0, 1},
+			[]string{"", "foo"},
+			"foo-6f64c6e626",
+			"multiple optional parts",
+		},
+		{
+			nil,
+			[]int{0, 1},
+			[]string{"foo", ""},
+			"foo-14fe48f0fb",
+			"multiple optional parts, different hash",
 		},
 	}
 	for _, c := range cases {
-		n := FromParts(Short, nil, nil, c.namespace, c.name)
+		n := FromParts(Short, c.constantIndices, c.optionalIndices, c.parts...)
 		if n != c.expected {
 			t.Errorf("%s failed: %s should be %s", c.description, n, c.expected)
 		}
 	}
-	// TODO test constant and optional indices
 }
