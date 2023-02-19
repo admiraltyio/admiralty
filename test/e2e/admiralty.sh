a6a7ac85c0e71aa3d2241993f72b1b088d366f84
adrienjt <adrienjt@users.noreply.github.com>
2023-02-19 00:50:16 -0800

    wait for sa secret to have token
    Signed-off-by: adrienjt <adrienjt@users.noreply.github.com>

--- test/e2e/admiralty.sh
+++ test/e2e/admiralty.sh
@@ -91,7 +91,7 @@ metadata:
     kubernetes.io/service-account.name: cluster$i
 type: kubernetes.io/service-account-token
 EOF
-      while ! k $j get sa cluster$i -n $ns; do sleep 1; done
+      while ! k $j get secret cluster$i -n $ns -o json | jq -e .data.token; do sleep 1; done
 
       TOKEN=$(k $j get secret cluster$i -n $ns -o json | jq -r .data.token | base64 --decode)
       KUBECONFIG=$(k $j config view --minify --raw -o json | jq '.users[0].user={token:"'$TOKEN'"} | .contexts[0].context.namespace="default"')
