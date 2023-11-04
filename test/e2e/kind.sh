26084e8ea6fc2ae6f4f2280090e23a694fb43beb
adrienjt <adrienjt@users.noreply.github.com>
2023-11-03 21:37:48 -0700

    add k8s 1.28 to e2e test
    Signed-off-by: adrienjt <adrienjt@users.noreply.github.com>

--- test/e2e/kind.sh
+++ test/e2e/kind.sh
@@ -23,6 +23,7 @@ source test/e2e/aliases.sh
 # https://github.com/kubernetes-sigs/kind/releases/tag/v0.20.0
 declare -A kind_images
 
+kind_images[1.28]="kindest/node:v1.28.0@sha256:b7a4cad12c197af3ba43202d3efe03246b3f0793f162afb40a33c923952d5b31"
 kind_images[1.27]="kindest/node:v1.27.3@sha256:3966ac761ae0136263ffdb6cfd4db23ef8a83cba8a463690e98317add2c9ba72"
 kind_images[1.26]="kindest/node:v1.26.6@sha256:6e2d8b28a5b601defe327b98bd1c2d1930b49e5d8c512e1895099e4504007adb"
 kind_images[1.25]="kindest/node:v1.25.11@sha256:227fa11ce74ea76a0474eeefb84cb75d8dad1b08638371ecf0e86259b35be0c8"
@@ -31,7 +32,7 @@ kind_images[1.23]="kindest/node:v1.23.17@sha256:59c989ff8a517a93127d4a536e7014d2
 kind_images[1.22]="kindest/node:v1.22.17@sha256:f5b2e5698c6c9d6d0adc419c0deae21a425c07d81bbf3b6a6834042f25d4fba2"
 kind_images[1.21]="kindest/node:v1.21.14@sha256:8a4e9bb3f415d2bb81629ce33ef9c76ba514c14d707f9797a01e3216376ba093"
 
-K8S_VERSION="${K8S_VERSION:-"1.27"}"
+K8S_VERSION="${K8S_VERSION:-"1.28"}"
 
 kind_setup() {
   i=$1
