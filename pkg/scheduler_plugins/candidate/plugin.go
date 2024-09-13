e23d64d60bb14b66df77faeb807c3aa58b8cf0d4
Marwan Ahmed <marwan@cohere.com>
2024-09-13 16:59:52 -0700

    regenerate code

--- pkg/scheduler_plugins/candidate/plugin.go
+++ pkg/scheduler_plugins/candidate/plugin.go
@@ -26,6 +26,7 @@ import (
 	"k8s.io/apimachinery/pkg/types"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/klog/v2"
 	"k8s.io/kubernetes/pkg/scheduler/framework"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 
