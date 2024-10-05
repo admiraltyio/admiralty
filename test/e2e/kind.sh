93972be9d37b87ca16c9b0238f38e196eff53bbc
Marwan Ahmed <marwan@cohere.com>
2024-10-05 02:38:13 -0700

    chore: include 1.31 in test matrix

--- test/e2e/kind.sh
+++ test/e2e/kind.sh
@@ -23,6 +23,7 @@ source test/e2e/aliases.sh
 # https://github.com/kubernetes-sigs/kind/releases/tag/v0.24.0
 declare -A kind_images
 
+kind_images[1.31]="kindest/node:v1.31.0@sha256:53df588e04085fd41ae12de0c3fe4c72f7013bba32a20e7325357a1ac94ba865"
 kind_images[1.30]="kindest/node:v1.30.4@sha256:976ea815844d5fa93be213437e3ff5754cd599b040946b5cca43ca45c2047114"
 kind_images[1.29]="kindest/node:v1.29.8@sha256:d46b7aa29567e93b27f7531d258c372e829d7224b25e3fc6ffdefed12476d3aa"
 kind_images[1.28]="kindest/node:v1.28.13@sha256:45d319897776e11167e4698f6b14938eb4d52eb381d9e3d7a9086c16c69a8110"
